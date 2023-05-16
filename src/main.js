6a55ae13bc0ac89bb1d83aad93fdd1a098a3781d
unknown <kamisawa@rayoo.jp>
2023-05-16 10:04:08 +0900

    Initial commit

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
  */
- 
+
 import Vue from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
--- src/pages/AI.vue
+++ src/pages/AI.vue
@@ -1,34 +1,3 @@
 <template>
-  <input id="check1" class="check" type="radio" name="ai" />
-  <label class="label" for="check1"> Translate - Amazon Translate </label>
-  <div class="content">
-  </div>
-
-  <input id="check2" class="check" type="radio" name="ai" />
-  <label class="label" for="check2"> Indentify Labels - Amazon Rekognition </label>
-  <div class="content">
-  </div>
-
-  <input id="check3" class="check" type="radio" name="ai" />
-  <label class="label" for="check3"> Indentify Text - Amazon Rekognition </label>
-  <div class="content">
-  </div>
-
-  <input id="check4" class="check" type="radio" name="ai" />
-  <label class="label" for="check4"> Interpret Text - Amazon Comprehend </label>
-  <div class="content">
-  </div>
-
-  <input id="check5" class="check" type="radio" name="ai" />
-  <label class="label" for="check5"> Text to Speech - Amazon Polly </label>
-  <div class="content">
-  </div>
+  <h1>AI View</h1>
 </template>
-
-<script>
-
-export default {
-  components: {
-  },
-};
-</script>
